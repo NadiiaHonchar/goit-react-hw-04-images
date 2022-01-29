388ebfe6704a526a985f5619f496629a5f1df29e
NadiiaHonchar <17nmg82@gmail.com>
2022-01-29 17:12:26 +0200

    my homework

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,42 +1,81 @@
-import React, { Component } from "react";
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import PropTypes from "prop-types";
 import style from "./Modal.module.css";
 
 const modalRoot = document.querySelector("#modal-root");
 
-class Modal extends Component {
-  static defaultProps = {
-    largeImage: PropTypes.string.isRequired,
-  };
+export default function Modal({ onClose, children }) {
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyDown);
-  }
-
-  handleKeyDown = (e) => {
+  const handleKeyDown = (e) => {
     if (e.code === "Escape") {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropClick = (e) => {
+  const handleBackdropClick = (e) => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <div className={style.Overlay} onClick={this.handleBackdropClick}>
-        <div className={style.Modal}>{this.props.children}</div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={style.Overlay} onClick={handleBackdropClick}>
+      <div className={style.Modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
 }
-export default Modal;
+
+Modal.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+// import React, { Component } from "react";
+// import { createPortal } from "react-dom";
+// import PropTypes from "prop-types";
+// import style from "./Modal.module.css";
+
+// const modalRoot = document.querySelector("#modal-root");
+
+// class Modal extends Component {
+//   static defaultProps = {
+//     largeImage: PropTypes.string.isRequired,
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener("keydown", this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener("keydown", this.handleKeyDown);
+//   }
+
+//   handleKeyDown = (e) => {
+//     if (e.code === "Escape") {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackdropClick = (e) => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <div className={style.Overlay} onClick={this.handleBackdropClick}>
+//         <div className={style.Modal}>{this.props.children}</div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
+// export default Modal;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,25 +1,20 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import { ImSearch } from "react-icons/im";
 import { toast } from "react-toastify";
 import style from "./Searchbar.module.css";
 
-class Searchbar extends Component {
-  state = {
-    searchName: "",
+export default function Searchbar({ onSubmit }) {
+  const [searchName, setSearchName] = useState("");
+
+  const handleInputChange = (e) => {
+    const enteredName = e.currentTarget.value.toLowerCase();
+    setSearchName(enteredName);
   };
 
-  static defaultProps = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-
-  handleInputChange = (e) => {
-    this.setState({ searchName: e.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (this.state.searchName.trim() === "") {
+    if (searchName.trim() === "") {
       toast.warn("please enter a request name", {
         position: "top-center",
         autoClose: 3000,
@@ -29,38 +24,106 @@ class Searchbar extends Component {
       });
       return;
     }
-    this.props.onSubmit(this.state);
-    this.reset();
-  };
-  reset = () => {
-    this.setState({ searchName: "" });
+    onSubmit(searchName);
+    reset();
   };
 
-  render = () => {
-    return (
-      <>
-        <header className={style.Searchbar}>
-          <form className={style.SearchForm} onSubmit={this.handleSubmit}>
-            <button type="submit" className={style.SearchFormButton}>
-              {" "}
-              <ImSearch />
-              <span className={style.SearchFormButtonLabel}>Search</span>
-            </button>
-
-            <input
-              className={style.SearchFormInput}
-              type="text"
-              autoComplete="off"
-              autoFocus={true}
-              placeholder="Search images and photos"
-              value={this.state.searchName}
-              onChange={this.handleInputChange}
-            />
-          </form>
-        </header>
-      </>
-    );
+  const reset = () => {
+    setSearchName("");
   };
+
+  return (
+    <>
+      <header className={style.Searchbar}>
+        <form className={style.SearchForm} onSubmit={handleSubmit}>
+          <button type="submit" className={style.SearchFormButton}>
+            {" "}
+            <ImSearch />
+            <span className={style.SearchFormButtonLabel}>Search</span>
+          </button>
+
+          <input
+            className={style.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus={true}
+            placeholder="Search images and photos"
+            value={searchName}
+            onChange={handleInputChange}
+          />
+        </form>
+      </header>
+    </>
+  );
 }
 
-export default Searchbar;
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+// import React, { Component } from "react";
+// import PropTypes from "prop-types";
+// import { ImSearch } from "react-icons/im";
+// import { toast } from "react-toastify";
+// import style from "./Searchbar.module.css";
+
+// class Searchbar extends Component {
+//   state = {
+//     searchName: "",
+//   };
+
+//   static defaultProps = {
+//     onSubmit: PropTypes.func.isRequired,
+//   };
+
+//   handleInputChange = (e) => {
+//     this.setState({ searchName: e.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (this.state.searchName.trim() === "") {
+//       toast.warn("please enter a request name", {
+//         position: "top-center",
+//         autoClose: 3000,
+//         closeOnClick: true,
+//         pauseOnHover: true,
+//         progress: 1,
+//       });
+//       return;
+//     }
+//     this.props.onSubmit(this.state);
+//     this.reset();
+//   };
+//   reset = () => {
+//     this.setState({ searchName: "" });
+//   };
+
+//   render = () => {
+//     return (
+//       <>
+//         <header className={style.Searchbar}>
+//           <form className={style.SearchForm} onSubmit={this.handleSubmit}>
+//             <button type="submit" className={style.SearchFormButton}>
+//               {" "}
+//               <ImSearch />
+//               <span className={style.SearchFormButtonLabel}>Search</span>
+//             </button>
+
+//             <input
+//               className={style.SearchFormInput}
+//               type="text"
+//               autoComplete="off"
+//               autoFocus={true}
+//               placeholder="Search images and photos"
+//               value={this.state.searchName}
+//               onChange={this.handleInputChange}
+//             />
+//           </form>
+//         </header>
+//       </>
+//     );
+//   };
+// }
+
+// export default Searchbar;
